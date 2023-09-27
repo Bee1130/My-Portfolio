3050032de389482650271ff84ccbd68714ba4fe6
Bee1130 <constructive.bee@gmail.com>
2023-09-27 04:32:08 -0400

    :hammer: add data

---
+++ data/data.js
@@ -0,0 +1,155 @@
+const DAJOUR = {
+  name: {
+    firstName: "Dajour",
+    lastName: "Walker"
+  },
+  avatar:
+    "https://lh3.googleusercontent.com/a/ACg8ocJWPJnDhV3ygj0MDwT2Rbp2yiWagUDvJrI9U8ZvHtYcvg=s288-c-no",
+  role: "Full Stack Developer & Project Manager",
+  summary: "",
+  skills: [
+    {
+      icon: "fa-react",
+      name: "React",
+      percent: 95
+    },
+    {
+      icon: "fa-node-js",
+      name: "NodeJS",
+      percent: 95
+    },
+    {
+      icon: "fa-python",
+      name: "Python",
+      percent: 70
+    },
+    {
+      icon: "fa-figma",
+      name: "Figma",
+      percent: 50
+    }
+  ],
+  statistic: {
+    experience: 4,
+    working: 40,
+    projects: 30
+  },
+
+  // Social Link Data
+  socials: [
+    {
+      id: "github",
+      title: "GitHub",
+      link: "https://github.com/melodyxpot"
+    },
+    {
+      id: "linkedin",
+      title: "Linkedin",
+      link: "https://linkedin.com/in/dajour-walker"
+    }
+  ],
+
+  portfolio: {
+    categories: ["OpenAI", "MERN", "Next.js", "Python"],
+    summary: "",
+    projects: [
+      {
+        id: "handmadeaiapp",
+        image:
+          "https://media.licdn.com/dms/image/D5622AQFb7KsWI09U4A/feedshare-shrink_800/0/1692105800259?e=1698883200&v=beta&t=ZU6_JIw3Q1OWHXenWKIvSc4aJYvgiceh4Wb8Z1uo0UE",
+        category: ["OpenAI", "Bubble"],
+        title: "Handmade AI",
+        link: "https://handmadeaiapp.com"
+      },
+      {
+        id: "chatwithfiles",
+        image:
+          "https://raw.githubusercontent.com/guangzhengli/ChatFiles/main/doc/chatfiles.png",
+        category: ["OpenAI", "Next.js"],
+        title: "Chatbot PDF Reader",
+        link: "https://chatbot.impactbuilder.app"
+      }
+    ]
+  },
+  services: [],
+
+  testimonials: [
+    {
+      id: "benleralph",
+      review: 5,
+      content: "",
+      avatar:
+        "https://media.licdn.com/dms/image/D5603AQHB8pKIINVqSA/profile-displayphoto-shrink_200_200/0/1694724958305?e=1700697600&v=beta&t=rFIKJrMdX_g33opz_hNfRVdJMOU9L7olbAAHQMVmhog",
+      name: "Ben Ralph",
+      role: "Founder in The Impact Positive Company"
+    },
+    {
+      id: "joshuabonner",
+      review: 5,
+      content: "",
+      avatar:
+        "https://directory.msmary.edu/people/people-photos/joshua-bonner_425px.jpg",
+      name: "Jorhua Bonner",
+      role: "Founder in BerryGoodMedia"
+    }
+  ],
+
+  resume: {
+    educations: [
+      {
+        school: "Fresno Pacific University",
+        date: "2017 - 2021",
+        title: "Bachelor's Degree"
+      }
+    ],
+    employments: [
+      {
+        title: "Full Stack Developer",
+        company: "Maze.Guru",
+        date: "2022 - 2023"
+      }
+    ]
+  },
+
+  clients: [
+    {
+      id: "theimpactpositivecompany",
+      link: "https://theimpactpositivecompany.com",
+      logo: "https://media.licdn.com/dms/image/D560BAQH87DrNJwpMxQ/company-logo_100_100/0/1682458827936?e=1703721600&v=beta&t=tbq8ZwaLNrmvy6Nu2v2txnTImFU4MVQRd8xCH3c2Ey8"
+    },
+    {
+      id: "berrygoodmedia",
+      link: "https://berrygoodmedia.org",
+      logo: "https://lh5.googleusercontent.com/p/AF1QipOtroG-D3C4X9Ki_tGJtF_8zSewDV6KAu3QsQwb=w160-h160-k-no"
+    }
+  ],
+
+  blogs: [
+    {
+      id: "how-to-create-a-chatgpt-application-using-next.js-and-the-openai-api",
+      image:
+        "https://miro.medium.com/v2/resize:fit:720/format:webp/1*yGUOMu9tXujdPRs2QfsV3Q.png",
+      date: "Jun 13",
+      title:
+        "How to create a ChatGPT application using Next.js and the OpenAI API.",
+      link: "https://medium.com/@melodyxpot/how-to-create-a-chatgpt-application-using-next-js-and-the-openai-api-63fa3b1cc4a3",
+      category: "OpenAI"
+    },
+    {
+      id: "How-to-Add-Speech-To-Text-Into-Your-Next.JS-App",
+      image:
+        "https://miro.medium.com/v2/resize:fit:640/format:webp/1*2DBFptS6_35PKXDpm7sxQg.png",
+      date: "Jun 13",
+      title: "How to Add Speech-To-Text Into Your Next.JS App",
+      link: "https://medium.com/@melodyxpot/how-to-add-speech-to-text-into-your-next-js-app-af7f008f9d7b",
+      category: "Next.js"
+    }
+  ],
+
+  contact: {
+    phone: "+(1) 815 853 6202",
+    email: "dajourjujuan.walker@gmail.com",
+    address: "2532 Grelyn Drive, Toledo, OH, USA",
+    calendly: "dajour-walker"
+  }
+};
